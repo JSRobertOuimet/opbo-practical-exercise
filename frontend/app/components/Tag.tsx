69f6d62cba684c98cc95cc2af3794764319fb6a9
JSRobertOuimet <jsrobertouimet@gmail.com>
2025-12-13 11:43:00 -0500

    Style checkmark icon.

--- frontend/app/components/Tag.tsx
+++ frontend/app/components/Tag.tsx
@@ -3,7 +3,7 @@ import { FaCircleCheck } from "react-icons/fa6";
 const Tag = ({ option }: { option: string }) => {
     return (
         <li key={option} className="align-center flex flex-row gap-2 text-sm">
-            <FaCircleCheck className="h-full text-green-500" />
+            <FaCircleCheck className="text-xtrek-teal h-full" />
             {option}
         </li>
     );
